50ccae5ecf738bf246f67e9d36d8eeee5ce002c1
sakanahong <sakanahong@gmail.com>
2026-03-04 00:29:39 +0800

    Complete Day 2 JavaScript practice

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
-function greet(name) {
-  return `Hello, ${name}`;
+const name2 = "ZhiHong";
+console.log(name2); 
+
+function add(a,b){
+    return a+b;
 }
 
-console.log(greet("Zhihong"));
+console.log(add(2,2));
+
+const foods = ["Chicken","Fish","Duck"];
+
+for (const food of foods){
+    console.log(name2,add(2,2),(food));
+}
+
---
+++ day2.js
@@ -0,0 +1,82 @@
+let name = "zhihong";
+let age = 29;
+let isLearning = true;
+
+console.log(name);
+console.log(age);
+console.log(isLearning);
+
+
+let city = "KualaLumpur";
+let score = 100;
+let passed = true;
+let emptyValue = null;
+
+console.log(city);
+console.log(score);
+console.log(passed);
+console.log(emptyValue);
+
+let a = 10;
+let b = 20;
+
+function add(c,d){
+    return c+d;
+}
+
+function deduct(c,d){
+    return c-d;
+}
+
+function multiply(c,d){
+    return c*d;
+}
+
+function devide(c,d){
+    return c/d;
+}
+
+let firstname = "Zhi";
+let lastname = "Hong";
+
+let fullname = firstname + " " + lastname
+
+console.log(fullname);
+
+let greeting = `Hello, ${fullname}`;
+console.log(greeting);
+
+
+function greet(veryfullname,age,home){
+    return `Hello, my name is ${veryfullname}. I'm ${age} years old and stay in ${home}`
+}
+
+let longage = 99;
+
+let colour = ["Red","Green"];
+console.log(colour);
+
+function add(a1,b1){
+    return `The total of A & B is ${a1+b1}`;
+}
+
+function multiply(a2,b2){
+    return `The multiply of A & B is ${a2*b2}`;
+}
+
+for (const loopcolour of colour){
+    if (Colour = "Red"){
+        console.log(add(a,b));
+    }
+    if(colour = "Green"){
+        console.log(multiply(a,b));
+    }
+}
+
+console.log(`Hello, my name is ${fullname} & i'm ${deduct(a,b)} this year & stay at ${city}. Do note that ${add(a,b)} `);
+
+
+
+
+
+
